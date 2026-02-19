18742108eb2af0a231f7abeb95c15fd86a2b9de2
Claire Coffey <clairecoffey@Claires-MacBook-Pro-2.local>
2026-02-19 21:03:40 +0000

    Switch site to Stone theme + fix Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 // https://astro.build/config
-export default defineConfig({  
-    site: 'https://clairecoffey.github.io',
+export default defineConfig({
+  site: 'https://clairecoffey.github.io',
+  experimental: {
+    viewTransitions: true,
+  },
+  integrations: [tailwind(), compress()],
 });
+
+
